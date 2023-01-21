c0f3ce84ef85dc11eba9e478448ce468e83c0c43
rafacollioni <rafa.collioni@gmail.com>
2023-01-21 15:48:48 -0300

    code basic cleanup

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ const domPasswordElement = document.querySelector('.generatedPassword');
 
 const allCharacters = {
   letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'w', 'z'],
-  simbols: ['!', '@', '#', '%', '&', '*'],
+  simbols: ['!', '@', '#', '%', , '&', '*'],
   numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
 };
 
 let passwordLenght = '12';
-
-let generatedPassword = '';
 
 passLenght.addEventListener('change', () => {
   passwordLenght = passLenght.value;
@@ -29,33 +27,34 @@ function startGenerator() {
 
   let usableCharacters = [];
 
+  // Scoped Function that Actualy Makes the Password;
   function generatePassword(usableCharacters) {
-    let randomPosition = Math.floor(Math.random() * usableCharacters.length);
-
-    generatedPassword = generatedPassword + usableCharacters[randomPosition];
+    let randomLetter = Math.floor(Math.random() * usableCharacters.length);
+    generatedPassword = generatedPassword + usableCharacters[randomLetter];
   }
 
-  // Only Letters Password
-
+  // Logic Operators to know what characters to use
   for (let i = 0; i < passwordLenght; i++) {
+    // SYMBOLS ❌  AND NUMBERS ❌
     if (symbols.checked == false && numbers.checked == false) {
       usableCharacters = allCharacters.letters;
-
       generatePassword(usableCharacters);
+
+      // SYMBOLS ✔️ AND NUMBERS ✔️
     } else if (numbers.checked == true && symbols.checked == true) {
       usableCharacters = allCharacters.letters.concat(allCharacters.numbers, allCharacters.simbols);
+      generatePassword(usableCharacters);
 
-      generatedPassword = generatedPassword + usableCharacters[Math.floor(Math.random() * usableCharacters.length)];
+      // SYMBOLS ✔️
     } else if (symbols.checked == true) {
       usableCharacters = allCharacters.letters.concat(allCharacters.simbols);
+      generatePassword(usableCharacters);
 
-      generatedPassword = generatedPassword + usableCharacters[Math.floor(Math.random() * usableCharacters.length)];
+      // NUMBERS ✔️
     } else if (numbers.checked == true) {
       usableCharacters = allCharacters.letters.concat(allCharacters.numbers);
-
-      generatedPassword = generatedPassword + usableCharacters[Math.floor(Math.random() * usableCharacters.length)];
+      generatePassword(usableCharacters);
     }
   }
-
   domPasswordElement.innerText = generatedPassword;
 }
